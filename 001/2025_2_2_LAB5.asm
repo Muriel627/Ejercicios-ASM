02fb816a78af23f2bf53ce17ac020850d2eebbca
Muriel627 <lupafor889@gmail.com>
2026-05-25 11:39:02 -0500

    sin enlazador

---
+++ 001/2025_2_2_LAB5.asm
@@ -0,0 +1,50 @@
+%define LOAD_ADDRESS 0x00020000
+%define CODE_SIZE END-(LOAD_ADDRESS+0x78)
+
+BITS 64
+org LOAD_ADDRESS
+ELF_HEADER:
+        db 0x7F, "ELF"
+        db 0x02
+        db 0x01
+        db 0x01
+        db 0x09
+        db 0x00
+        times 7 db 0x00
+        dw 0x0002
+        dw 0x003E
+        dd 0x00000001
+        dq _start
+        dq 0x0000000000000040
+        dq 0x0000000000000000
+        dd 0x00000000
+        dw 0x0040
+        dw 0x0038
+        dw 0x0001
+        dw 0x0000
+        dw 0x0000
+        dw 0x0000
+PROGRAM_HEADER:
+        dd 0x00000001
+        dd 0x00000007
+        dq 0x0000000000000078
+        dq LOAD_ADDRESS+0x78
+        dq 0x0000000000000000
+        dq CODE_SIZE
+        dq CODE_SIZE
+        dq 0x0000000000000000
+
+section .text
+global _start
+_start:
+        mov AL, 5
+        mov BL, 3
+        mov CL, 10
+lazito:
+        mul RBX
+        loop lazito
+adios:
+        mov RAX, 60
+        mov RDI, 0
+        syscall
+END:
